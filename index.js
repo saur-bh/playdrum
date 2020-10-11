2b8fe09c4fe22f46f361539ab366cf518497cfac
Saurabh Verma <saurabh-verma@outlook.com>
2020-10-11 17:58:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,36 +32,36 @@ function handlePress(e){
 
       switch(soundCliked.trim()){
         case 'w':
-                audio.src = '/sounds/tom-1.mp3'
+                audio.src = 'sounds/tom-1.mp3'
                 
                 audio.play();
                    break;
 
         case 'a':
-                audio.src = '/sounds/tom-2.mp3'
+                audio.src = 'sounds/tom-2.mp3'
                 audio.play();
                 break;
 
         case 's':
-                audio.src = '/sounds/tom-3.mp3'
+                audio.src = 'sounds/tom-3.mp3'
                 audio.play();
                 break;
 
         case 'd':
-                audio.src = '/sounds/tom-4.mp3'
+                audio.src = 'sounds/tom-4.mp3'
                 audio.play();
                 break;
 
         case 'j':
-                audio.src = '/sounds/snare.mp3'
+                audio.src = 'sounds/snare.mp3'
                 audio.play();
                 break;
         case 'k':
-                audio.src = '/sounds/crash.mp3'
+                audio.src = 'sounds/crash.mp3'
                 audio.play();
                 break;
         case 'l':
-                audio.src = '/sounds/kick-bass.mp3'
+                audio.src = 'sounds/kick-bass.mp3'
                 audio.play();
                 break;
                 
